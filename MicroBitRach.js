ad5ed5f75a8dd2903e4355becae30d9f19b400e2
Rach Allen <rachel.m.allen@outlook.com>
2021-11-16 14:28:36 -0600

    Changed for switch

--- MicroBitRach.js
+++ MicroBitRach.js
@@ -1,8 +1,9 @@
-input.onButtonPressed(Button.A, function () {
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    servos.P0.setAngle(180)
-})
-input.onButtonPressed(Button.B, function () {
-    servos.P0.setAngle(0)
-})
 servos.P0.setAngle(0)
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+        servos.P0.setAngle(180)
+    }
+    if (pins.digitalReadPin(DigitalPin.P1) == 0) {
+        servos.P0.setAngle(0)
+    }
+})
