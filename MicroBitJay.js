ddb81c69c9ee7f2e1e91f91591d67adbfc416ff7
Rach Allen <rachel.m.allen@outlook.com>
2021-11-29 17:38:59 -0600

    Fixed Radio to Rach and Jay, added sounds for testing

--- MicroBitJay.js
+++ MicroBitJay.js
@@ -2,8 +2,9 @@ input.onButtonPressed(Button.A, function () {
     servos.P1.setAngle(180)
 })
 radio.onReceivedString(function (receivedString) {
-    if (receivedString == "Trade") {
-        servos.P1.setAngle(180)
+    if (receivedString == "Cow") {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        servos.P1.setAngle(0)
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -13,6 +14,7 @@ servos.P1.setAngle(0)
 radio.setGroup(17)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        music.playTone(784, music.beat(BeatFraction.Whole))
         radio.sendString("Beans")
     }
 })
