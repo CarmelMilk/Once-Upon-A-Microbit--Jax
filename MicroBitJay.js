6ea06fdf7a4c3174370e650f7351c4045e33fa77
Rach Allen <rachel.m.allen@outlook.com>
2021-11-30 13:50:27 -0600

    Final Rach and Jay

--- MicroBitJay.js
+++ MicroBitJay.js
@@ -6,7 +6,6 @@ radio.onReceivedString(function (receivedString) {
         gocow = 1
     }
     if (receivedString == "Cow" && gocow == 1) {
-        music.playTone(262, music.beat(BeatFraction.Whole))
         servos.P1.setAngle(0)
     }
 })
@@ -16,10 +15,8 @@ input.onButtonPressed(Button.B, function () {
 let gocow = 0
 servos.P1.setAngle(0)
 radio.setGroup(17)
-gocow = 0
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
-        music.playTone(784, music.beat(BeatFraction.Whole))
         radio.sendString("Beans")
     }
 })
--- MicroBitRach.js
+++ MicroBitRach.js
@@ -3,7 +3,6 @@ input.onButtonPressed(Button.A, function () {
 })
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "Beans") {
-        music.playTone(131, music.beat(BeatFraction.Whole))
         servos.P1.setAngle(180)
     }
 })
@@ -14,7 +13,7 @@ servos.P1.setAngle(0)
 radio.setGroup(17)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
-        music.playTone(220, music.beat(BeatFraction.Whole))
         radio.sendString("Cow")
     }
 })
+
